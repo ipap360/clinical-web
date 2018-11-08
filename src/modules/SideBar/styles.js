d83bb8492826d8930989e8f877b61720bb98458f
Ioannis Papadopoulos <ipap360@gmail.com>
2018-11-08 11:17:48 +0200

    drawer css

--- src/modules/SideBar/styles.js
+++ src/modules/SideBar/styles.js
@@ -1,6 +1,14 @@
+const drawerWidth = 300;
 export default theme => {
     return {
-        root: {},
+        root: {
+            "& > div": {
+                width: drawerWidth - theme.spacing.unit,
+                padding: theme.spacing.unit * 4,
+                // position: "relative",
+                borderRight: "none"
+            }
+        },
         main: {}
     };
 };
